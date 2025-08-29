9f268bc34bce2c6a315e3e01faddac465e4fe129
Mohamed <md.diag75@gmail.com>
2025-08-29 11:30:13 +0100

    add istio gateway for prometheus

--- 0-notes.sh
+++ 0-notes.sh
@@ -21,8 +21,8 @@ helm template istio-ingress istio/gateway -n istio-ingress > istio_gateway.yaml
 kubectl apply -f istio_gateway.yaml
 
 ## Prometheus operator
-helm template prometheus-operator oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -n prometheus-operator --include-crds --output-dir ./gitops-apps/prometheus_operator
-helm template prometheus-operator oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -n prometheus-operator --include-crds > ./prometheus_operator/prometheus_operator.yaml
+    #helm template prometheus-operator oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -n prometheus-operator --include-crds --output-dir ./gitops-apps/prometheus_operator
+    #helm template prometheus-operator oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -n prometheus-operator --include-crds > ./prometheus_operator/prometheus_operator.yaml
 
 ## Prometheus community
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
